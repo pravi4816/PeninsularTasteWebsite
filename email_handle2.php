d7c90e252f3e64092151f024cceabda10c1524a4
pravi4816 <65004426+pravi4816@users.noreply.github.com>
2020-07-02 15:24:16 +0530

    Add files via upload

---
+++ email_handle2.php
@@ -0,0 +1,16 @@
+<?
+$name = $_REQUEST['name'];
+$phone = $_REQUEST['phone'];
+$msg = $_REQUEST['msg'];
+
+if(empty($name) || empty($phone) || empty($msg))
+{
+	echo "Please fill all the details";
+}
+else {
+	mail("pravi4816@gmail.com","Peninsular_customer", $msg, "From: $name <$email>");
+	echo "<script type='text/javascript'>alert('Thanks for you feedback. we will contact you soon');
+		window.history.log(-1);
+		</script>";
+}
+?>
